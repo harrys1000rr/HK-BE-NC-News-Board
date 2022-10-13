41766df510a40f9563987ee16561edd611820e3c
harrys1000rr <harry@DESKTOP-NC80NV3.localdomain>
2022-10-13 13:53:53 +0100

    redone number 6 on new branch

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -3,7 +3,6 @@ const request = require("supertest");
 const app = require("../app.js");
 const testData = require("../db/data/test-data/index.js");
 const seed = require("../db/seeds/seed.js");
-require('jest-sorted');
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
@@ -51,7 +50,7 @@ describe("GET /api/users", () => {
 
 
 describe('GET - /api/articles/:article_id', () => {
-  test('GET - when given a valid parametric, will return status 200- ', () => {
+  test('GET - when given a valid ID, will return status 200 along with respective article. ', () => {
     return request(app)
       .get(`/api/articles/2`)
       .expect(200)
@@ -69,88 +68,6 @@ describe('GET - /api/articles/:article_id', () => {
         );
       });
   });
-
-  test('GET comment count- ', () => {
-    return request(app)
-      .get(`/api/articles/9`)
-      .expect(200)
-      .then((res) => {
-        expect(res.body.article).toEqual(
-          expect.objectContaining({ comment_count: expect.any(String) })
-        );
-        expect(res.body.article.comment_count).toBe('2');
-      });
-  });
-});
-
-describe("6. PATCH /api/articles/:article_id", () => {
-  test("responds with status 200 and specific article is updated", () => {
-    return request(app)
-      .patch("/api/articles/2")
-      .send({ inc_votes: 50 })
-      .expect(200)
-      .then((res) => {
-        expect(res.body.article).toEqual(
-          expect.objectContaining({
-            article_id: 2,
-            title: expect.any(String),
-            author: expect.any(String),
-            body: expect.any(String),
-            topic: expect.any(String),
-            created_at: expect.any(String),
-            votes: expect.any(Number)
-          })
-        );
-      });
-  });
-  
-});
-
-
-describe("8. GET /api/articles?topic", () => {
-    test("200: returns an array of all articles with correct comment count (sorted by date descending as default) when passed no queries", async () => {
-      const res = await request(app)
-      .get("/api/articles").expect(200);
-      expect(res.body.articles.length).toBe(12);
-      expect(res.body.articles).toBeSortedBy("created_at", {
-        descending: true,
-      });
-      res.body.articles.forEach((article) => {
-        console.log(Object.keys(article))
-        expect(Object.keys(article)).toEqual([
-          "article_id",
-          "title",
-          "topic",
-          "author",
-          "body",
-          "created_at",
-          "votes",
-          "comment_count"
-        ]);
-      });
-    });
-    test('200: returns array filtered by topic when given a topic query', async () => {
-      const res = await request(app)
-          .get('/api/articles?topic=mitch')
-          .expect(200)
-      expect(res.body.articles.length).toBe(11);
-      expect(res.body.articles).toBeSortedBy('created_at', { descending: true });
-  });
-  
-  test('404: returns empty array when given topic query that does not exist', async () => {
-    const res = await request(app)
-      .get("/api/articles?topic=BIGMANHARRY")
-        .expect(404);
-        expect(res.body.msg).toEqual("Articles not found");
-  
-  });
-  
-  });
-
-  
-
-// ------------ Error Handling ------------//
-describe("Error Handling", () => {
   test("404: show user that URL is invalid", () => {
     return request(app)
       .get("/api/BigManTing")
@@ -173,6 +90,50 @@ test("status:404, responds with a 404 error when passed a article id which does 
     .get("/api/articles/111")
     .expect(404)
     .then(({ body }) => {
-      expect(body.msg).toEqual("Articles not found");
+      expect(body.msg).toEqual("Article with this ID not found.");
     });
 });
+
+
+describe("6. PATCH /api/articles/:article_id", () => {
+  test.only("200 returns the specified article with updated vote count", () => {
+    return request(app)
+      .patch("/api/articles/2")
+      .send({ inc_votes: 50 })
+      .expect(200)
+      .then((res) => {
+        expect(res.body.article).toEqual(
+          expect.objectContaining({
+            article_id: 2,
+            title: expect.any(String),
+            author: expect.any(String),
+            body: expect.any(String),
+            topic: expect.any(String),
+            created_at: expect.any(String),
+            votes: 50
+          })
+        );
+      });
+  });
+  test("404, returns 404 error when passed a article id which does not exist in the database.", () => {
+    return request(app)
+      .get("/api/articles/111")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toEqual("Articles not found");
+      });
+  });
+  test('400: returns 400 when invalid article_id datatype provided', () => {
+    return request(app)
+      .get('/api/articles/SELECTA!')
+      .expect(400)
+      .then((res) => {
+        expect(res.body).toEqual({ msg: 'Invalid id type!' });
+      });
+  });
+
+});
+  
+
+
+
--- models/model.js
+++ models/model.js
@@ -50,4 +50,16 @@ exports.selectArticles = (topic) => {
   });
   };
      
-  
+  exports.patchArticleById = (article_id, inc_votes) => {return db
+    .query(`UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *`, [
+      inc_votes,
+      article_id,
+    ])
+    .then((result) => {
+        if (result.rows.length === 0) {
+
+            return Promise.reject({ status: 400, msg: 'Article with this ID not found.'});
+          }
+          return result.rows[0];
+        });
+    };
