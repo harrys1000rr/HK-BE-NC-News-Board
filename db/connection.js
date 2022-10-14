6e8db8740b7aa66333a1650902b5892e5a75ea87
harrys1000rr <harry@DESKTOP-NC80NV3.localdomain>
2022-10-14 15:21:49 +0100

    heroku shizzle

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,14 @@
 const { Pool } = require('pg');
+
 const ENV = process.env.NODE_ENV || 'development';
+const config = ENV === 'production'
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
